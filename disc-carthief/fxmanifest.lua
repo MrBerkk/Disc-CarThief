7189a1ab2c24ffbbf019812abc1650079414ace2
MrBerk <60743320+nova1238@users.noreply.github.com>
2021-01-17 19:14:00 +0300

    Add files via upload

---
+++ disc-carthief/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Car Thief'
+
+version '1.0.0'
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'config.lua',
+    'server/main.lua'
+}
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'config.lua',
+    'client/main.lua'
+}
